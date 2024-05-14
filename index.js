1e01c64c0452642fdc9174bb1e6c7f9145b34cbe
hoangminhcong001 <jrmfischerpun0suy@hotmail.com>
2024-05-14 11:44:16 +0700

    demo task commit

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-const linearSearch = (arr, target) => {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === target) {
-      return i;
+const combSort = (arr) => {
+  const shrinkFactor = 1.3;
+  let gap = arr.length;
+  let swapped = true;
+  while (gap > 1 || swapped) {
+    gap = Math.floor(gap / shrinkFactor);
+    if (gap < 1) {
+      gap = 1;
+    }
+    swapped = false;
+    let i = 0;
+    while (i + gap < arr.length) {
+      if (arr[i] > arr[i + gap]) {
+        [arr[i], arr[i + gap]] = [arr[i + gap], arr[i]];
+        swapped = true;
+      }
+      i++;
     }
   }
-  return -1;
+  return arr;
 };
